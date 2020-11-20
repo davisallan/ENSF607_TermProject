e57ad810c6719278c95231281df184f87dbbe3d8
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-11-20 12:58:55 -0700

    'Added credentials for Santiago DB'

--- src/PreProjectExercise/IDBCredentials.java
+++ src/PreProjectExercise/IDBCredentials.java
@@ -6,8 +6,12 @@ public interface IDBCredentials {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost/inventorydb";
 
-    //  Database credentials
-    static final String USERNAME = "davis";
-    static final String PASSWORD = "Davisallan123!";
+    //  Database credentials Davis
+//    static final String USERNAME = "davis";
+//    static final String PASSWORD = "Davisallan123!";
+
+    // Database credentials Santiago
+    static final String USERNAME = "santiago";
+    static final String PASSWORD = "santiago123!";
 
 }
