b403bac9f992b2486942ff6e204a6fdb1472df50
davisallan <davis.allan@ucalgary.ca>
2020-11-22 18:51:00 -0700

    implemented initialize connection, added credentials

--- src/Server/Controller/DBController/DBController.java
+++ src/Server/Controller/DBController/DBController.java
@@ -1,4 +1,43 @@
 package Server.Controller.DBController;
 
-public class DBController {
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class DBController implements DBCredentials{
+
+    private Connection conn;
+    private PreparedStatement stmt;
+    private ResultSet rs;
+
+    public void initializeConnection() {
+        try {
+            //Register JDBC driver
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            //Open a connection
+            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Problem");
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            stmt.close();
+            rs.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
---
+++ src/Server/Controller/DBController/DBCredentials.java
@@ -0,0 +1,11 @@
+package Server.Controller.DBController;
+
+public interface DBCredentials {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/toolshop";
+
+    //  Database credentials
+    static final String USERNAME = "davis";
+    static final String PASSWORD = "Davisallan123!";
+}
