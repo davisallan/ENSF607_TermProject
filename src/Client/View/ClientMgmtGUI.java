217f6e5ea5bbe5e71141a7c76ec4c3aceee3b2e1
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-11-23 16:00:18 -0700

    Creates JFrame

--- src/Client/View/ClientMgmtGUI.java
+++ src/Client/View/ClientMgmtGUI.java
@@ -1,4 +1,15 @@
 package Client.View;
 
+import javax.swing.*;
+
 public class ClientMgmtGUI {
+
+    public ClientMgmtGUI() {
+        JFrame frame = new JFrame("Tool Shop App");
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
