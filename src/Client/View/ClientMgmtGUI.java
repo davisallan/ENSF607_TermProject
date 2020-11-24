30b92ba36263131c876c806cb938305774d8e102
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-11-23 21:42:46 -0700

    Created GUI

--- src/Client/View/ClientMgmtGUI.java
+++ src/Client/View/ClientMgmtGUI.java
@@ -13,11 +13,28 @@ public class ClientMgmtGUI extends JFrame{
         frame.setSize(500, 500);
         frame.setLocation(300,100);
 
+        // Left panel
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+        // Title label "Search Clients
         JLabel search = new JLabel("Search Clients");
         search.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         search.setFont(search.getFont().deriveFont(20f));
         leftPanel.add(search);
+        // Select type label
+        JLabel type = new JLabel("Select type of search to be performed:");
+        leftPanel.add(type);
+        // Client ID option
+        JCheckBox id = new JCheckBox("Client ID");
+        leftPanel.add(id);
+        // Last Name option
+        JCheckBox name = new JCheckBox("Last Name");
+        leftPanel.add(name);
+        // Client Type option
+        JCheckBox clientType = new JCheckBox("Client Type");
+        leftPanel.add(clientType);
+        // Search parameters label
+        JLabel param = new JLabel("Enter the search parameters below:");
+        leftPanel.add(param);
 
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
         JLabel client = new JLabel("Client Information");
