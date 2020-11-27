7689e589a9d0beee49ff1b44e1e0ae2e75281ebe
santiagofloresm <64857391+santiagofloresm@users.noreply.github.com>
2020-11-26 22:42:51 -0700

    Added more queries to the presentation file

--- DBFiles/presentation.sql
+++ DBFiles/presentation.sql
@@ -3,22 +3,24 @@ USE toolshop;
 # 1. From other file
 
 # 2. A basic retrieval query
-# Displaying all suppliers
-SELECT * FROM supplier;
+# Displaying all tools that cost more than 40
+SELECT T.tName, T.sName, T.price 
+FROM tool AS T 
+WHERE price > 40;
 
 # 3. A retrieval query with ordered results
-# Displaying all tools ordered by price descending
-SELECT * FROM tool ORDER BY price DESC;
+# Displaying tools ordered by price descending
+SELECT T.tName, T.tType, T.price FROM tool AS T ORDER BY price DESC;
 
 # 4. A nested retrieval query
-# Displaying all non-electrical tools that have less than 40 units in stock
-SELECT T.tName, T.quantity, T.sName
+# Displaying the name, price and supplier name of the tools that customer 138 has bought
+SELECT T.tName, T.price, T.sName
 FROM tool AS T
-WHERE quantity IN 
-	 (SELECT quantity
-      FROM tool
-      WHERE tType="non-electrical"
-			AND quantity < 40);
+WHERE T.toolId IN 
+	 (SELECT P.toolId
+      FROM purchases AS P
+      WHERE P.toolId=T.toolId
+			AND P.customerId = 138);
             
 # 5. A retrieval query using joined tables
 # Getting all tools (from left table) and Power Type from right table, will show null if left not in right
